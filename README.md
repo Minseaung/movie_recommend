26e60298bbd849f3523903c0b5081ed248e537a2
lalala <lalala@naver.com>
2021-07-05 17:31:34 +0900

    user_list

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Object" 
+"# movie_recommend" 
---
+++ do_crwaring.py
@@ -0,0 +1,9 @@
+import crwaring
+
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+
+page = 13323069
+
+while page > 13321069 :
+    page = str(page)
